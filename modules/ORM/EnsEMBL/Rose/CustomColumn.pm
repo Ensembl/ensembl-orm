d95d91b404494fae0b5d453f837417df88f79daf
Harpreet Riat <hr5@sanger.ac.uk>
2013-05-14 16:05:49 +0000

    init_custom_column method splitted into 2 method; names added to triggers; sprintf typo fixed

--- modules/ORM/EnsEMBL/Rose/CustomColumn.pm
+++ modules/ORM/EnsEMBL/Rose/CustomColumn.pm
@@ -8,32 +8,49 @@ use warnings;
 
 use ORM::EnsEMBL::Utils::Exception;
 
-sub init_custom_column {
+sub new_from_existing {
+  ## Returns a new column object after modifying the existing one
+  ## @param Existing column object
+  ## @return Custom column object (or undef if not successful)
   my $class = shift;
   my $self  = $_[0];
 
   if (ref $self && UNIVERSAL::isa($self, 'Rose::DB::Object::Metadata::Column')) { # if trying to convert an existing column to a datastructure column
 
     my @allowed_columns = $class->allowed_base_classes;
-    throw(sprintf 'Only %s type columns can be converted to %s type columns', join(', ' map { $_->type } @allowed_columns), $class->type) unless grep $self->isa($_), @allowed_columns;
+    throw(sprintf 'Only %s type columns can be converted to %s type columns', join(', ', map { $_->type } @allowed_columns), $class->type) unless grep $self->isa($_), @allowed_columns;
 
     $self = bless $self, $class;
     $self->init(%{$_[1] || {}});
-  } else {
-    $self = $class->SUPER::new(@_);
+
+    return $self;
   }
+}
+
+sub init_custom_column {
+  ## Initialises the inflate and deflate triggers on the column values for faking them as the custom column type
+  ## @return Custom column object
+  my $self = shift;
 
   # Add triggers to modify column values
-  $self->add_trigger('inflate' => sub {
-    my ($object, $value) = @_;
-    return $self->value_class->new($value, $self);
-  });
+  $self->add_trigger(
+    'event' => 'inflate',
+    'name'  => 'value_to_value_class',
+    'code'  => sub {
+      my ($object, $value) = @_;
+      return $self->value_class->new($value, $self);
+    }
+  );
 
-  $self->add_trigger('deflate' => sub {
-    my ($object, $value) = @_;
-    $value = $self->value_class->new($value, $self) unless UNIVERSAL::isa($value, $self->value_class);
-    return $value->to_string;
-  });
+  $self->add_trigger(
+    'event' => 'deflate',
+    'name'  => 'value_class_to_value',
+    'code'  => sub {
+      my ($object, $value) = @_;
+      $value = $self->value_class->new($value, $self) unless UNIVERSAL::isa($value, $self->value_class);
+      return $value->to_string;
+    }
+  );
 
   return $self;
 }
@@ -41,13 +58,13 @@ sub init_custom_column {
 sub allowed_base_classes {
   ## Returns a list of classes that can be converted into the given custom column type
   ## Override this in child class to return the appropriate class names
-  throw(sprintf 'Allowed base classes have not been defined for ', ref $_[0] || $_[0]);
+  throw(sprintf 'Allowed base classes have not been defined for %s', ref $_[0] || $_[0]);
 }
 
 sub value_class {
   ## Returns the name of the class that will be instantiated to represent value of this column
   ## Override this in child class to return the appropriate class name
-  throw(sprintf 'Custom column value class has not been defined for ', ref $_[0] || $_[0]);
+  throw(sprintf 'Custom column value class has not been defined for %s', ref $_[0] || $_[0]);
 }
 
 sub type {
