712729240f09896ba30edc9bf7d6c5c33c5e185e
Harpreet Riat <hr5@sanger.ac.uk>
2013-05-14 11:37:35 +0000

    exception class to provide try, catch, throw features

---
+++ modules/ORM/EnsEMBL/Utils/Exception.pm
@@ -0,0 +1,57 @@
+package ORM::EnsEMBL::Utils::Exception;
+
+use strict;
+use warnings;
+
+use Carp qw(longmess);
+
+use overload qw("" to_string);
+
+sub import {
+  ## Imports the try, catch and throw function to the calling package
+  my $class  = shift;
+  my $caller = caller;
+
+  {
+    # import functions
+    no strict qw(refs);
+
+    *{"${caller}::try"} = sub (&$) {
+      my ($try, $catch) = @_;
+      eval { &$try };
+      if ($@) {
+        local $_ = $@;
+        local $@ = undef;
+        &$catch;
+      }
+    };
+
+    *{"${caller}::catch"} = sub (&) {
+      shift;
+    };
+
+    *{"${caller}::throw"} = sub {
+      die $class->new(@_);
+    };
+  }
+}
+
+sub new {
+  my ($class, $message) = @_;
+  return bless {
+    'message' => $message  || '',
+    'stack'   => longmess
+  }, $class;
+}
+
+sub to_string {
+  my $self = shift;
+  return sprintf "Uncaught exception '%s' with message '%s'\n  %s", $self->type, $self->{'message'}, $self->{'stack'};
+}
+
+
+sub type {
+  return 'ORMException';
+}
+
+1;
