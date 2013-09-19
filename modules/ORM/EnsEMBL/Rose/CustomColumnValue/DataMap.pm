c46144f5da8ac14cd69c5f4dc75912460bc2dff9
Harpreet Riat <hr5@sanger.ac.uk>
2013-09-19 17:43:13 +0000

    'deflate' method added to set default values before actually calling parent class's deflate method

--- modules/ORM/EnsEMBL/Rose/CustomColumnValue/DataMap.pm
+++ modules/ORM/EnsEMBL/Rose/CustomColumnValue/DataMap.pm
@@ -1,6 +1,5 @@
 package ORM::EnsEMBL::Rose::CustomColumnValue::DataMap;
 
-## Name: ORM::EnsEMBL::Rose::CustomColumnValue::DataMap
 ## Class representing the value provided to column type 'datamap'
 
 use strict;
@@ -10,14 +9,22 @@ use ORM::EnsEMBL::Utils::Exception;
 use base qw(ORM::EnsEMBL::Rose::CustomColumnValue::DataStructure);
 
 sub new {
-  ## @overrides
+  ## @override
   ## @constructor
   ## @exception If data is not a hash 
-  my ($class, $data, $trusted) = @_;
-  my $self = $class->SUPER::new($data || {}, $trusted);
+  my ($class, $data) = splice @_, 0, 2;
+  my $self = $class->SUPER::new($data || {}, @_);
   throw('Column of type datatype can only accept stringified hash values.') unless $self->isa('HASH');
 
   return $self;
+}
+
+sub deflate {
+  ## @override
+  ## Adds default values to the virtual keys before deflating the column value
+  my ($self, $column) = splice @_, 0, 2;
+  $column->set_default_values($self);
+  return $self->SUPER::deflate($column, @_);
 }
 
 sub has_key {
