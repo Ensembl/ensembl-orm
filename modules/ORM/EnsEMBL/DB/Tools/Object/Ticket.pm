bef169e7dabcaae3faa1b77b30e4db961ad7b157
Harpreet Riat <hr5@sanger.ac.uk>
2013-10-01 09:16:50 +0000

    auto-initialize the object from DB

--- modules/ORM/EnsEMBL/DB/Tools/Object/Ticket.pm
+++ modules/ORM/EnsEMBL/DB/Tools/Object/Ticket.pm
@@ -3,49 +3,13 @@ package ORM::EnsEMBL::DB::Tools::Object::Ticket;
 use strict;
 use warnings;
 
+use ORM::EnsEMBL::DB::Tools::Object::Job; # for foreign key initialisation
+
 use base qw(ORM::EnsEMBL::DB::Tools::Object);
 
 __PACKAGE__->meta->setup(
-  table       => 'ticket',
-
-  columns     => [
-    ticket_id     => {type => 'serial', primary_key => 1, not_null => 1},
-    owner_id      => {type => 'varchar', length => 32, not_null => 1},
-    owner_type    => {type => 'enum', 'values' => [qw(user session)], not_null => 1, 'default' => 'session'},
-    job_type_id   => {type => 'tinyint', not_null=>1},
-    ticket_name   => {type => 'varchar', length => 32, not_null => 1},
-    ticket_desc   => {type => 'varchar', length =>160},
-    created_at    => {type => 'timestamp', not_null => 1},
-    modified_at   => {type => 'timestamp', not_null => 1},
-    status        => {type => 'enum', 'values' => [qw(Queued Current Pending Running Parsing Completed Deleted Failed)], 'default' => 'Queued'},
-    status_report => {type => 'text'},
-    site_type     => {type => 'varchar', length => 32, not_null => 1},
-  ],
-
-  relationships => [
-    analysis => {
-      type        =>  'one to one',
-      class       =>  'ORM::EnsEMBL::DB::Tools::Object::Analysis',
-      column_map  =>  {'ticket_id' => 'ticket_id'},
-    },
-    job_type => {
-      type        => 'one to one',
-      class       => 'ORM::EnsEMBL::DB::Tools::Object::JobType',
-      column_map  => {'job_type_id' => 'job_type_id'},
-    },
-    sub_job => {
-      type        => 'one to many',
-      class       => 'ORM::EnsEMBL::DB::Tools::Object::SubJob',
-      column_map  => {'ticket_id' => 'ticket_id'},
-    },
-    result => {
-      type        => 'one to many',
-      class       => 'ORM::EnsEMBL::DB::Tools::Object::Result',
-      column_map  => {'ticket_id' => 'ticket_id'},
-    },
-  ],
-
+  table           => 'ticket',
+  auto_initialize => []
 );
 
 1;
-
