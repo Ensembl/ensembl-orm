a945a1471f2e60a7ff2f2dfaf6362c844e4e62cf
Harpreet Riat <hr5@sanger.ac.uk>
2013-10-01 08:49:35 +0000

    unwanted methods removed; new method 'is_ticket_name_unique' added

--- modules/ORM/EnsEMBL/DB/Tools/Manager/Ticket.pm
+++ modules/ORM/EnsEMBL/DB/Tools/Manager/Ticket.pm
@@ -8,37 +8,10 @@ use base qw(ORM::EnsEMBL::Rose::Manager);
 
 sub object_class { 'ORM::EnsEMBL::DB::Tools::Object::Ticket' };
 
-sub fetch_ticket_by_name {
-  my ($self, $ticket_name) = @_;
-
-  return $ticket_name ? $self->get_objects(
-    with_objects => ['job_type'],
-    query        => ['ticket_name' => $ticket_name ],
-    limit        => 1
-  )->[0] : undef;
-}
-
-sub fetch_all_tickets_by_user {
-  my ($self, $user_id) = @_;
-
-  return unless $user_id;
-
-  return $self->get_objects(
-    with_objects  => ['job_type'],
-    query         => ['owner_id' => $user_id, 'owner_type' => 'user' ]
-  );
-}
-
-sub fetch_all_tickets_by_session {
-  my ($self, $session_id) = @_;
-
-  return unless $session_id;
-
-  return $self->get_objects(
-    with_objects  => ['job_type'],
-    query         => ['owner_id' => $session_id, 'owner_type' => 'session' ]
-  );
+sub is_ticket_name_unique {
+  ## @return 1 is name is unique, 0 otherwise
+  my ($self, $name) = @_;
+  return $self->get_objects_count('select' => ['ticket_name'], 'query' => [ 'ticket_name' => $name ]) ? 0 : 1;
 }
 
 1;
-
