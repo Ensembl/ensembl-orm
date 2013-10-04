875278556c6a17da4919d12bb54d19d91bc5414c
Harpreet Riat <hr5@sanger.ac.uk>
2013-10-04 10:53:51 +0000

    an extra field 'trackable' added to the 'register_db' method to optionally turn off trackability of the table data

--- modules/ORM/EnsEMBL/Rose/DbConnection.pm
+++ modules/ORM/EnsEMBL/Rose/DbConnection.pm
@@ -1,11 +1,11 @@
 package ORM::EnsEMBL::Rose::DbConnection;
 
-### NAME: ORM::EnsEMBL::Rose::DbConnection
 ### Subclass of Rose::DB, a wrapper around DBI to provide some default
 
 use strict;
 use warnings;
 
+use ORM::EnsEMBL::Rose::DbEntry;
 use ORM::EnsEMBL::Utils::Exception;
 
 use base qw(Rose::DB);
@@ -16,19 +16,32 @@ __PACKAGE__->default_domain('ensembl'); ## Set the default domain
 sub register_database {
   ## Adds db configurations to the registery
   ## @param Hashref with following keys
-  ##  - database Database name
-  ##  - host     Database server address
-  ##  - port     Port number (default to 3306)
-  ##  - username Database user name
-  ##  - password Passowrd
-  ##  - driver   Database driver type (defaults to mysql)
-  ##  - type     Type as specified in individual rose objects
+  ##  - database  Database name
+  ##  - host      Database server address
+  ##  - port      Port number (default to 3306)
+  ##  - username  Database user name
+  ##  - password  Passowrd
+  ##  - driver    Database driver type (defaults to mysql)
+  ##  - type      Type as specified in individual rose objects
+  ##  - trackable Flag if off, will not respect the trackable info (on by default)
   my ($self, $params) = @_;
 
-  return $self->register_db(qw(port 3306 driver mysql domain ensembl), %$params);
+  return $self->register_db(ORM::EnsEMBL::Rose::DbEntry->new(qw(port 3306 driver mysql domain ensembl trackable 1), %$params));
+}
+
+sub trackable {
+  ## Gets info about whether this database should respect the trackable info or not (readonly)
+  ## Rose::DB calls this method to set the value in the first place (while instantiating this class) as specified in the register_db() arguments
+  ## @return 1 or 0
+  my $self    = shift;
+  my $caller  = caller;
+  $self->{'trackable'} = shift if @_ && $caller eq 'Rose::DB';
+  return $self->{'trackable'};
 }
 
 sub dbi_connect {
+  ## @override
+  ## Overrides the default dbi_connect method to throw an exception if connection couldn't be established
   my ($self, @args) = @_;
   my $connection;
   try {
