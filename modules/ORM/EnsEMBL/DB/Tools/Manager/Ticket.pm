2fae5489079b11431a01602428e6324e34843b22
Harpreet Riat <harpreet@ebi.ac.uk>
2014-02-23 14:48:42 +0000

    New method count_current_tickets added

--- modules/ORM/EnsEMBL/DB/Tools/Manager/Ticket.pm
+++ modules/ORM/EnsEMBL/DB/Tools/Manager/Ticket.pm
@@ -27,7 +27,27 @@ sub object_class { 'ORM::EnsEMBL::DB::Tools::Object::Ticket' };
 sub is_ticket_name_unique {
   ## @return 1 is name is unique, 0 otherwise
   my ($self, $name) = @_;
-  return $self->get_objects_count('select' => ['ticket_name'], 'query' => [ 'ticket_name' => $name ]) ? 0 : 1;
+  return $self->get_objects_count('query' => [ 'ticket_name' => $name ]) ? 0 : 1;
+}
+
+sub count_current_tickets {
+  ## Counts all the tickets for the given user_id and session_id
+  ## @param Hashref with keys:
+  ##  - site_type   : Site type string value as defined in SiteDefs ENSEMBL_SITE_TYPE (required)
+  ##  - type        : Type string (or arrayref of strings) for filtering the TicketType (optional)
+  ##  - user_id     : ID of the user to filter the tickets with (optional)
+  ##  - session_id  : Session id to filter the tickets with (optional)
+  ## @return Integer count
+  my ($self, $params) = @_;
+
+  my $site_type = delete $params->{'site_type'} or throw("Please provide 'site_type' in the arguments.");
+  my $query     = [ map { exists $params->{"${_}_id"} ? [ 'owner_id' => $params->{"${_}_id"}, 'owner_type' => $_ ] : () } qw(user session) ];
+     $query     = @$query > 1 ? [ 'or' => [ map {('and' => $_)} @$query ] ] : $query->[0];
+
+  push @$query, 'ticket_type_name'  => $params->{'type'} if exists $params->{'type'};
+  push @$query, 'site_type'         => $site_type;
+
+  return $self->get_objects_count('query' => $query);
 }
 
 1;
