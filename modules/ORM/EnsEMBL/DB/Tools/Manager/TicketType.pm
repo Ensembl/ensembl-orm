26d2640831c8df3f45e9cdff82c59883f3876e28
Harpreet Riat <harpreet@ebi.ac.uk>
2015-02-03 12:28:47 +0000

    Method modified to return jobs on request

--- modules/ORM/EnsEMBL/DB/Tools/Manager/TicketType.pm
+++ modules/ORM/EnsEMBL/DB/Tools/Manager/TicketType.pm
@@ -62,9 +62,9 @@ sub fetch_with_requested_ticket {
   ## @param Hashref with keys:
   ##  - site_type     : Site type string value as defined in SiteDefs ENSEMBL_SITE_TYPE (required)
   ##  - ticket_name   : Ticket name (required)
-  ##  - job_id        : Job id of the required job (if provided only this job will be returned, otherwise all jobs will be returned)
+  ##  - job_id        : Job id of the required job or 'all' if all jobs are required (if not provided or value is false, no job objects will be returned)
   ##  - type          : Type string (or arrayref of strings) for filtering the TicketType (optional)
-  ##  - result_id     : Result id if only one result is required, 'all' if all are required (if key not provided, no result objects are returned)
+  ##  - result_id     : Result id if only one result is required, 'all' if all are required (if key not provided or value is false, no result objects are returned)
   ##  - user_id       : ID of the user to filter the ticket (required if session_id is missing)
   ##  - session_id    : Session id to filter the ticket (required if user_id is missing)
   ##  - public_ok     : Flag if on, will return the ticket even if it doesn't belong to the given user or session but has visibility 'public'
@@ -83,11 +83,12 @@ sub fetch_with_requested_ticket {
   # at least one out of session and user is required
   throw("Please provide 'session_id' or 'user_id' in the arguments.") unless $query;
 
-  push @$query, ('ticket.job.result.result_id' => $params->{'result_id'} || 0) if $params->{'result_id'} && $params->{'result_id'} ne 'all';
-  push @$query, ('ticket.ticket_name' => $ticket_name, 'ticket.site_type' => $site_type, $params->{'job_id'} ? ('ticket.job.job_id' => $params->{'job_id'}) : (), $ticket_type_name ? ('ticket_type_name' => $ticket_type_name) : ());
+  push @$query, ('ticket.job.job_id' => $params->{'job_id'}) if $params->{'job_id'} && $params->{'job_id'} ne 'all';
+  push @$query, ('ticket.job.result.result_id' => $params->{'result_id'}) if $params->{'result_id'} && $params->{'result_id'} ne 'all';
+  push @$query, ('ticket.ticket_name' => $ticket_name, 'ticket.site_type' => $site_type, $ticket_type_name ? ('ticket_type_name' => $ticket_type_name) : ());
 
   return $self->get_objects(
-    'with_objects'  => ['ticket', 'ticket.job', $params->{'result_id'} ? 'ticket.job.result' : ()],
+    'with_objects'  => ['ticket', $params->{'job_id'} ? 'ticket.job' : (), $params->{'result_id'} ? 'ticket.job.result' : ()],
     'multi_many_ok' => 1,
     'query'         => $query
   )->[0];
