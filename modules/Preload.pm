0d4f77e23f5c0e71de726a499b12d4b62d9dd7f0
Dan Sheppard <dan@ebi.ac.uk>
2015-11-05 13:05:47 +0000

    Preload ROSE objects for child init speed.

---
+++ modules/Preload.pm
@@ -0,0 +1,12 @@
+package Preload;
+
+use ORM::EnsEMBL::DB::Tools::Manager::TicketType;
+use ORM::EnsEMBL::DB::Tools::Manager::Ticket;
+use ORM::EnsEMBL::DB::Tools::Object;
+use ORM::EnsEMBL::DB::Tools::Object::JobMessage;
+use ORM::EnsEMBL::DB::Tools::Object::Job;
+use ORM::EnsEMBL::DB::Tools::Object::Result;
+use ORM::EnsEMBL::DB::Tools::Object::Ticket;
+use ORM::EnsEMBL::DB::Tools::Object::TicketType;
+
+1;
