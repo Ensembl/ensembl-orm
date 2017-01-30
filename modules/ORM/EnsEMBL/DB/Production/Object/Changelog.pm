d041fedf488e836a622c490b82b93842d18efd60
Jyothish <joyjoy.nt@gmail.com>
2017-01-30 13:54:12 +0000

    New DOI category for REST

--- modules/ORM/EnsEMBL/DB/Production/Object/Changelog.pm
+++ modules/ORM/EnsEMBL/DB/Production/Object/Changelog.pm
@@ -46,7 +46,7 @@ __PACKAGE__->meta->setup(
     db_type_affected      => {type => 'set',  'values' => [qw(cdna core funcgen otherfeatures rnaseq variation vega)]},
     mitochondrion         => {type => 'enum', 'values' => [qw(Y N changed)], 'default' => 'N'},
     priority              => {type => 'integer', 'not_null' => 1, 'default' => 1},
-    category              => {type => 'enum', 'values' => [qw(genebuild variation regulation alignment web schema retired other)], 'default' => 'other'},
+    category              => {type => 'enum', 'values' => [qw(genebuild variation regulation alignment web schema retired rest other)], 'default' => 'other'},
     is_current            => {type => 'integer', 'not_null' => 1, 'default' => 1},
     site_type             => {type => 'enum', 'values' => [qw(ensembl grch37 pre)], 'default' => 'ensembl'}
   ],
